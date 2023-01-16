0565b4d6146ba1ddf2e24bab0a569013f5ff4a24
mannysanz715 <mannysanz715@gmail.com>
2023-01-16 00:47:25 -0800

    Create Models for profile

--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,24 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const calorieSchema = new Schema({
+  caloriesDay: Number,
+  caloriesWeek: Number,
+  calorieLimit: Number,
+  caloriesBurned: Number
+})
+
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  height: Number,
+  weight: Number,
+  calories:[calorieSchema],
+  age: Number,
+  sex: String,
+  workouts: [{type: Schema.Types.ObjectId , ref: 'Workout'}],
+  meals: [{ type: Schema.Types.ObjectId, ref: 'Meal'}],
+  
 }, {
   timestamps: true
 })
