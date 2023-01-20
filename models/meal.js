e5a5b6e385e8344f6b40d11ace643d4e97d8b541
MannySanz <mannysanz715@gmail.com>
2023-01-20 10:53:41 -0800

    Meal Authetication of deletion

--- models/meal.js
+++ models/meal.js
@@ -1,10 +1,11 @@
-import mongoose from 'mongoose'
+import mongoose, { isObjectIdOrHexString } from 'mongoose'
 
 const Schema = mongoose.Schema
 
 const mealSchema = new Schema({
   name: String,
   calories: Number,
+  owner: String,
 }, {
   timestamps: true,
 })
